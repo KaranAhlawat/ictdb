01a8ccba60770fe2a191b125ff0793b69fd42f7f
Karan Ahlawat <ahlawatkaran12@gmail.com>
2025-01-19 01:19:20 +0530

    fix formatting

--- app/src/io/karan/ictdb/views/Layout.scala
+++ app/src/io/karan/ictdb/views/Layout.scala
@@ -3,23 +3,12 @@ package io.karan.ictdb.views
 import scalatags.Text.all.*
 import scalatags.Text.tags2.nav
 
-object Layout {
+object Layout:
   def apply[A](content: Seq[Modifier]) =
     html(
       head(
         title := "ICTdb",
         link(rel := "stylesheet", href := "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css")
       ),
-      body(
-        header(
-          nav(
-            p("ICTdb")
-          )
-        ),
-        content,
-        footer(
-          p("Made with ❤\uFE0F with Scala + HTMX")
-        )
-      )
+      body(header(nav(p("ICTdb"))), content, footer(p("Made with ❤\uFE0F with Scala + HTMX")))
     )
-}
