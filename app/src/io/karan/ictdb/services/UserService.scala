62220e13b57f88465b72e9baff9bc09804e62371
Karan Ahlawat <ahlawatkaran12@gmail.com>
2025-03-24 00:24:00 +0530

    add: Password hashing

--- app/src/io/karan/ictdb/services/UserService.scala
+++ app/src/io/karan/ictdb/services/UserService.scala
@@ -1,6 +1,7 @@
 package io.karan.ictdb.services
 
 import cats.effect.IO
+import com.password4j.Password
 import io.karan.ictdb.domain.User
 import io.karan.ictdb.persistence.UserRepo
 import io.scalaland.chimney.dsl.*
@@ -13,13 +14,17 @@ trait UserService:
 
 class UserServiceLive private[services] (userRepo: UserRepo) extends UserService:
   override def registerUser(user: User) =
-    userRepo.delay(userRepo.insert(user.transformInto[UserCreator]))
+    val hashedPw    = user.userPassword.map(pw => Password.hash(pw).addRandomSalt(12).withScrypt.getResult)
+    val updatedUser = user.copy(userPassword = hashedPw)
+    userRepo.delay(userRepo.insert(updatedUser.transformInto[UserCreator]))
 
   override def loginUser(usernameOrEmail: String, password: String): IO[Either[String, User]] =
     val user = userRepo.delay(userRepo.findByUsernameOrEmail(usernameOrEmail))
     user.map: userOpt =>
       userOpt.fold(Left("Invalid credentials provided"))(user =>
-        if user.userPassword.contains(password) then Right(user.transformInto[User])
+        val passwordMatches = user.userPassword.map(hash => Password.check(password, hash).withScrypt).contains(true)
+
+        if passwordMatches then Right(user.transformInto[User])
         else Left("Invalid credentials provided")
       )
 
